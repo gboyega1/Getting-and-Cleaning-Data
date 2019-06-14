35d4b03abe0a9368a9799a1cf48f0cead6e5d908
gboyega1 <30779057+gboyega1@users.noreply.github.com>
2019-06-14 05:29:19 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -13,6 +13,6 @@ mean_stdv_data <- Merged_data[grepl("\\<mean()\\>", colnames(Merged_data)) | gre
 mean_stdv_data$Activities <- activity_labels$V2[mean_stdv_data$Activities]
 colnames(mean_stdv_data) <- mgsub(c(colnames(mean_stdv_data)), c('\\()|_|-','^t'), c('',"Time"))
 mean_std_dat <- group_by(mean_stdv_data, SubjectID, Activities) %>% 
-summarise_all(funs(mean))
+summarise_all(list(mean))
 write.table(mean_std_dat, "mean_std_dat.txt", sep = "\t", row.name = FALSE)
 }
